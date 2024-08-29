83fe48ce2c764902bf4ce01725bfcd9e4a1d00e2
abdelhamid2004 <abdelhamideslm900@gmail.com>
2024-08-29 11:20:38 +0300

    Acc

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,36 +1,40 @@
 #!/usr/bin/python3
 """Solves the lock boxes puzzle """
 
-def x(i, vis,boxes):
-     """Check if all boxes can be opened by dfs
+
+def x(i, vis, boxes):
+    """Check if all boxes can be opened by dfs
     Args:
-    	i (int): List child
+    i (int): List child
         vis (list): List to check visited
         boxes (list): List which contain all the boxes with the keys
     Returns:
         bool: True if all boxes can be opened, otherwise, False
     """
-    vis[i]=1
+
+    vis[i] = 1
     for it in boxes[i]:
         if vis[it] == 0:
-            x(it,vis,boxes)
+            x(it, vis, boxes)
 
 
 def canUnlockAll(boxes):
-     """Check if all boxes can be opened
+    """Check if all boxes can be opened
     Args:
         boxes (list): List which contain all the boxes with the keys
     Returns:
         bool: True if all boxes can be opened, otherwise, False
-        """ 
-    vis=[]
+        """
+
+    vis = []
     for i in range(0, len(boxes)):
         vis.append(0)
-    x(0,vis,boxes)
+    x(0, vis, boxes)
     for i in range(0, len(vis)):
         if vis[i] == 0:
             return False
     return True
+
 
 def main():
     """Entry point"""
